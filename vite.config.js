b2e14c1730bcf4f60c557f7cf70087a28c9679b3
fenixdj3 <152913107+fenixdj3@users.noreply.github.com>
2026-06-30 01:33:27 +0500

    fix: add missing comma in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/border-radius-previewer'
+  base: '/border-radius-previewer/',
   server: {
     port: 3000,
     open: true
